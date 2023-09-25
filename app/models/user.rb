5e8abff8bab020d9d9f4073172d9acdc8ab88413
matuyama <143941798+matuyama@users.noreply.github.com>
2023-09-25 13:32:27 +0000

    [finish]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,22 @@ class User < ApplicationRecord
     following_users.include?(user)
   end
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
+
+
   has_one_attached :profile_image
 
   validates :name, {presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true}
