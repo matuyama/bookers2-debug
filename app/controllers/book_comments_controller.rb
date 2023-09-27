b585e3c28c93888f892ddecea5dc1cebd69a73af
matuyama <143941798+matuyama@users.noreply.github.com>
2023-09-27 01:15:54 +0000

    [finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -10,7 +10,7 @@ class BookCommentsController < ApplicationController
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path
+    redirect_to book_path(params[:book_id])
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,19 @@
 class FavoritesController < ApplicationController
 
+
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to user_path(book.user)
+    # redirect_to user_path(book.user)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to user_path(book.user)
+    # redirect_to user_path(book.user)
   end
 
 end
