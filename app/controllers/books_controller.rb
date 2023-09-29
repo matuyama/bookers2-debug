59e45ed00fd527dc49d07d59f7672ec5dc957f48
matuyama <143941798+matuyama@users.noreply.github.com>
2023-09-29 00:55:30 +0000

    [add]応用課題9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,14 @@ class BooksController < ApplicationController
     @book_comment = BookComment.new
     @book = Book.find(params[:id])
     @user = @book.user
+    # 閲覧数
+    @book_detail = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.view_counts.create(book_id: @book_detail.id)
+    end
+    # unless ViewCount.where(created_at: Time.zone.now.all_day).find_by(user_id: current_user.id, book_id: @book.id)
+    #   current_user.view_counts.create(book_id: @book_detail.id)
+    # end
   end
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ApplicationRecord
   validates :name, {presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true}
   validates :introduction, length: {maximum: 50 }
 
-
+  has_many :view_counts, dependent: :destroy
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
